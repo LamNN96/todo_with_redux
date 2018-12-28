a0219d35e99f095cc85cbf4cf399071a15347ce9
lamnn96 <lamnn.dev@gmail.com>
2018-12-28 16:15:00 +0700

    remove unused code and comment

--- App.js
+++ App.js
@@ -1,61 +1,9 @@
 import React, { Component } from "react";
-import { createAppContainer, createStackNavigator } from "react-navigation";
-import { Provider, connect } from "react-redux";
-import { createStore, applyMiddleware, combineReducers } from "redux";
-import {
-  reduxifyNavigator,
-  createReactNavigationReduxMiddleware,
-  createNavigationReducer
-} from "react-navigation-redux-helpers";
-import TaskDetail from "./src/components/TaskDetail";
-import TaskReducer from "./src/reducers/TaskReducer";
-import AddTaskContainer from "./src/containers/AddTaskContainer";
-import TodoListContainer from "./src/containers/TodoListContainer";
-import EditTaskContainer from "./src/containers/EditTaskContainer";
+import { Provider } from "react-redux";
 import configureStore from "./src/store/store";
 import AppWithNavigationState from "./src/navigation/AppWithNavigationState";
-// const AppNavigator = createStackNavigator(
-//   {
-//     Home: {
-//       screen: TodoListContainer
-//     },
-//     Detail: {
-//       screen: TaskDetail
-//     },
-//     EditTask: {
-//       screen: EditTaskContainer
-//     },
-//     AddTask: {
-//       screen: AddTaskContainer
-//     }
-//   },
-//   {
-//     initialRouteName: "Home"
-//   }
-// );
-
-// const navReducer = createNavigationReducer(AppNavigator);
-
-// const rootReducer = combineReducers({
-//   nav: navReducer,
-//   tasks: TaskReducer
-// });
-
-// const middleWare = createReactNavigationReduxMiddleware(
-//   "root",
-//   state => state.nav
-// );
-
-// const store = createStore(rootReducer, applyMiddleware(middleWare));
-
-// const AppReduxify = reduxifyNavigator(AppNavigator, "root");
-// const mapStateToProps = state => ({
-//   state: state.nav
-// });
 
 const store = configureStore();
-
-// const AppWithNavigationState = connect(mapStateToProps)(AppReduxify);
 
 export default class App extends Component {
   render() {
